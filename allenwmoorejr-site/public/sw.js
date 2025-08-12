cfaa4bc26ce0647961efa82c3d7ffffc5e084391
Allen Moore <allenwmoorejr@outlook.com>
2025-08-11 21:08:12 -0500

    removed nested repo

--- allenwmoorejr-site/public/sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='clm-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest'])))});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
